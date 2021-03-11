a60ddcf9c48b14e9c7ee10eb3c9e83d12dfd00fa
Suliaman Abass <suliaman.abass46@gmail.com>
2021-03-11 12:40:42 +0100

    Added a bit more stuff xD. I like this whole committing and pushing stuff feels like I'm doing something important

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import random
 
 
 def gradient(matrix, vector, omega):
-    return np.add((np.dot((matrix.T + matrix), omega)), vector)
+    return np.add((np.dot((matrix.T + matrix), omega)),
+                  vector)  # Gradient of general quadratic function (A trans + A)w +b
 
 
-def get_j(a, b, c, x):
+def get_j(a, b, c, x):  # Calculating value of function at x
     return c + np.add(np.dot(b.T, x), np.dot(np.dot(x.T, a), x))
 
 
@@ -23,12 +24,12 @@ def gradient_descent(tolerance, learning_rate, max_iter, a, b, c, x0):
 
 
 def hessian(a):
-    return np.add(a, a.T)
+    return np.add(a, a.T) # Calculating hessian of quadratic function A + A^T
 
 
 def newton(tolerance, max_iter, a, b, c, x0):
     j = get_j(a, b, c, x0)
-    inv_hess = np.linalg.inv(hessian(a))
+    inv_hess = np.linalg.inv(hessian(a))   # Inverse Hessian as required in the Newton Method
     count = 0
     while count < max_iter and j > tolerance:
         x = x0 - np.dot(gradient(a, b, x0), inv_hess)
@@ -36,40 +37,6 @@ def newton(tolerance, max_iter, a, b, c, x0):
         x0 = x
         j = get_j(a, b, c, x0)
     return x0
-
-
-# v = [1, 2, 3]
-# m = np.array([[1, 2, 3], [3, 4, 5], [7, 8, 9]])
-# w = [3, 3, 5]
-
-# print(gradient(m, v, w))
-# xx = newton(0.00001, 10, np.array([[1, 0], [0, 1]]), np.array([1, 0]), 1, np.array([3, 1]))
-# print(xx)
-# print(get_j(np.array([[1, 0], [0, 1]]), np.array([1, 0]), 1, xx))
-# print(gradient_descent(0.00001, 0.5, 10, np.array([[1, 0], [0, 1]]), np.array([1, 0]), 1, np.array([3, 1])))
-
-# print('Enter Numbers')
-# entries = list(map(float, input().split()))
-# x_now = np.array(entries)
-# print(x_now)
-#
-# print('Enter Numbers')
-# entries = list(map(float, input().split()))
-# b = np.array(entries)
-# print(b)
-#
-# print('Enter Numbers')
-# entries = list(map(float, input().split()))
-# a = np.array(entries).reshape(2, 2)
-# print(a)
-#
-# print('Enter range')
-# entries = list(map(float, input().split()))
-# var = []
-# for i in range(len(b)):
-#     var.append(random.uniform(entries[0], entries[1]))
-#
-# print(var)
 
 
 def get_func_coeff(dim):
@@ -106,26 +73,29 @@ def get_param():
     tol = float(input())
     print('Enter max iterations')
     max_iter = int(input())
-    print('Enter learning rate (Enter 0 is not applicable)')
+    print('Enter learning rate (Enter 0 if not applicable)')
     learning_rate = float(input())
 
     return tol, max_iter, learning_rate
 
 
 def run_methods(a, b, c, x):
+    solution = {}
     tol, max_iter, learning_rate = get_param()
     print('Enter 1 to run gradient Descent OR 2 to run Newtons method OR 3 to run both')
-    path = int(input())
-    if path == 1 or path == 3:
-        sol = gradient_descent(tol, learning_rate, max_iter, a, b, c, x)
-        print('Solution found by gradient descent:')
-        print(sol)
-    if path == 2 or path == 3:
-        sol = newton(tol, max_iter, a, b, c, x)
-        print('Solution found by Newton:')
-        print(sol)
-    if path != 1 and path != 2 and path != 3:
-        print('Incorrect Option chosen')
+    while True:
+        path = int(input())
+        if path == 1 or path == 3:
+            sol = gradient_descent(tol, learning_rate, max_iter, a, b, c, x)
+            solution['Solution by Gradient Descent:  '] = sol
+        if path == 2 or path == 3:
+            sol = newton(tol, max_iter, a, b, c, x)
+            solution['Solution by Newton Method: '] = sol
+        if path != 1 and path != 2 and path != 3:
+            print('Incorrect Option chosen')
+            continue
+        return solution
+
 
 def batch_mode(coeff, params, dim, n, entries):
     a, b, c = coeff
@@ -138,14 +108,14 @@ def batch_mode(coeff, params, dim, n, entries):
             x.append(random.uniform(entries[0], entries[1]))
         sol_vec_newton.append(newton(tol, max_iter, a, b, c, np.array(x)))
         sol_vec_grad.append(gradient_descent(tol, learning_rate, max_iter, a, b, c, np.array(x)))
-        # run_methods(a, b, c, np.array(x))
     print('STD and MEAN for Newton \n')
     print(np.std(sol_vec_newton, axis=0))
     print(np.mean(sol_vec_newton, axis=0))
     print('STD and MEAN for Gradient Descent \n')
     print(np.std(sol_vec_grad, axis=0))
     print(np.mean(sol_vec_grad, axis=0))
-    # print(np.std(sol_vec,axis=1))
+
+
 
 # MAIN INTERFACE
 
@@ -161,8 +131,9 @@ while True:
 
         a, b, c = get_func_coeff(dim)
 
-        run_methods(a, b, c, x_now)
-
+        solution = run_methods(a, b, c, x_now)
+        for i in solution:
+            print(i, solution[i])
         break
 
     elif path == 2:
@@ -170,17 +141,17 @@ while True:
         dim = int(input())
         print('Enter range for x separated by space')
         entries = list(map(float, input().split()))
-        print('Enter 1 to run methods OR 2 for Batch mode')  
+        print('Enter 1 to run methods OR 2 for Batch mode')
         while True:
             path = int(input())
             if path == 1:
-                # print('Enter range for x separated by space')
-                # entries = list(map(float, input().split()))
                 x = []
                 for i in range(dim):
                     x.append(random.uniform(entries[0], entries[1]))
                 a, b, c = get_func_coeff(dim)
-                run_methods(a, b, c, np.array(x))
+                solution = run_methods(a, b, c, np.array(x))
+                for i in solution:
+                    print(i, solution[i])
                 break
             elif path == 2:
                 print('Enter n - number of time to run methods')
@@ -190,11 +161,3 @@ while True:
             print('Incorrect option entered try again')
         break
     print('Incorrect option entered please try again')
-
-
-
-
-
-
-
-
