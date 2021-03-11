bb179ae754f5ed88d633b0ba36800d85ded43a1a
Suliaman Abass <suliaman.abass46@gmail.com>
2021-03-11 10:27:55 +0100

    Implementing Batch Mode and cleaning up main interface

--- main.py
+++ main.py
@@ -98,7 +98,8 @@ def get_func_coeff(dim):
 
     print('Enter c')
     c = float(input())
-    return a,b,c
+    return a, b, c
+
 
 def get_param():
     print('Enter tolerance')
@@ -106,15 +107,17 @@ def get_param():
     print('Enter max iterations')
     max_iter = int(input())
     print('Enter learning rate (Enter 0 is not applicable)')
-    learning_rate =float(input())
+    learning_rate = float(input())
 
-    return tol,max_iter,learning_rate
-def run_methods(a,b,c,x):
-    tol,max_iter,learning_rate = get_param()
+    return tol, max_iter, learning_rate
+
+
+def run_methods(a, b, c, x):
+    tol, max_iter, learning_rate = get_param()
     print('Enter 1 to run gradient Descent OR 2 to run Newtons method OR 3 to run both')
     path = int(input())
-    if  path == 1 or path == 3:
-        sol = gradient_descent(tol,learning_rate,max_iter,a,b,c,x)
+    if path == 1 or path == 3:
+        sol = gradient_descent(tol, learning_rate, max_iter, a, b, c, x)
         print('Solution found by gradient descent:')
         print(sol)
     if path == 2 or path == 3:
@@ -124,35 +127,74 @@ def run_methods(a,b,c,x):
     if path != 1 and path != 2 and path != 3:
         print('Incorrect Option chosen')
 
-
+def batch_mode(coeff, params, dim, n, entries):
+    a, b, c = coeff
+    tol, max_iter, learning_rate = params
+    sol_vec_newton = []
+    sol_vec_grad = []
+    for i in range(n):
+        x = []
+        for i in range(dim):
+            x.append(random.uniform(entries[0], entries[1]))
+        sol_vec_newton.append(newton(tol, max_iter, a, b, c, np.array(x)))
+        sol_vec_grad.append(gradient_descent(tol, learning_rate, max_iter, a, b, c, np.array(x)))
+        # run_methods(a, b, c, np.array(x))
+    print('STD and MEAN for Newton \n')
+    print(np.std(sol_vec_newton, axis=0))
+    print(np.mean(sol_vec_newton, axis=0))
+    print('STD and MEAN for Gradient Descent \n')
+    print(np.std(sol_vec_grad, axis=0))
+    print(np.mean(sol_vec_grad, axis=0))
+    # print(np.std(sol_vec,axis=1))
 
 # MAIN INTERFACE
 
 print('Enter 1 to manually add all data OR 2 to choose range for x/ Batch mode')
-path = int(input())
-if path == 1:
-    print('Enter dimension')
-    dim = int(input())
-    print('Enter numbers for x separated by space')
-    entries = list(map(int, input().split()))
-    x_now = np.array(entries)
-
-    a,b,c = get_func_coeff(dim)
-
-    run_methods(a,b,c,x_now)
-
-elif path == 2:
-    print('Enter dimension')
-    dim = int(input())
-    print('Enter 1 to choose range for x OR 2 for Batch mode') # Batch mode not implemented yet
+while True:
     path = int(input())
     if path == 1:
+        print('Enter dimension')
+        dim = int(input())
+        print('Enter numbers for x separated by space')
+        entries = list(map(int, input().split()))
+        x_now = np.array(entries)
+
+        a, b, c = get_func_coeff(dim)
+
+        run_methods(a, b, c, x_now)
+
+        break
+
+    elif path == 2:
+        print('Enter dimension')
+        dim = int(input())
         print('Enter range for x separated by space')
         entries = list(map(float, input().split()))
-        x = []
-        for i in range(dim):
-            x.append(random.uniform(entries[0], entries[1]))
-        a,b,c = get_func_coeff(dim)
-        run_methods(a,b,c,np.array(x))
-else:
-    print('Incorrect option entered')
+        print('Enter 1 to run methods OR 2 for Batch mode')  
+        while True:
+            path = int(input())
+            if path == 1:
+                # print('Enter range for x separated by space')
+                # entries = list(map(float, input().split()))
+                x = []
+                for i in range(dim):
+                    x.append(random.uniform(entries[0], entries[1]))
+                a, b, c = get_func_coeff(dim)
+                run_methods(a, b, c, np.array(x))
+                break
+            elif path == 2:
+                print('Enter n - number of time to run methods')
+                n = int(input())
+                batch_mode(get_func_coeff(dim), get_param(), dim, n, entries)
+                break
+            print('Incorrect option entered try again')
+        break
+    print('Incorrect option entered please try again')
+
+
+
+
+
+
+
+
